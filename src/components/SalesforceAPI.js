35fb27a9b87119bb5f4b01e73bb0420d093d80f2
Isaac Contreras <iacontrerasg@icloud.com>
2020-03-23 15:46:09 -0600

    Update SalesforceAPI.js

--- src/components/SalesforceAPI.js
+++ src/components/SalesforceAPI.js
@@ -17,17 +17,18 @@ export default class SalesforceAPI {
   }
 
   salesforceQuery(query) {
+    var cuerpo = {
+      token: localStorage.getItem("sfToken"),
+      instance:
+        localStorage.getItem("sfInstance") + "/services/data/v42.0/query/?q=",
+      query: query
+    };
+
     return this.fetch(
       "https://api.ccscontactcenter.com/v1/auth/salesforceQuery",
       {
         method: "POST",
-        body: {
-          token: localStorage.getItem("sfToken"),
-          instance:
-            localStorage.getItem("sfInstance") +
-            "/services/data/v42.0/query/?q=",
-          query: query
-        }
+        body: JSON.stringify(cuerpo)
       }
     ).then(res => {
       return Promise.resolve(res);
